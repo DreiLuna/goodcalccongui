4e645284183103c2865e2f40a643bcf52cf4a9e8
DreiLuna <54549805+DreiLuna@users.noreply.github.com>
2022-02-09 15:19:59 -0600

    multidigit

--- tkniterpracv2.py
+++ tkniterpracv2.py
@@ -6,143 +6,190 @@ ddc = 0
 global user_num1
 user_num1= 0
 
-def ex0(ddc, user_num1):
-    ddc = ddc + 1
+def ex0():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
 
     if ddc != 2:
-        user_num1 = 0
-    elif ddc == 2:
-        user_num1_ram = user_num1 * 10
-
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
         
-def ex1(ddc, user_num1):
-    ddc = ddc + 1
+        user_num1 = 0
+        print(user_num1)
+    elif ddc == 2:
+        user_num1 = user_num1 * 10
+
+        user_num1 = user_num1 + 0
+        print (user_num1)
+        
+def ex1():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
 
     if ddc != 2:
         user_num1 = 1
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 1
+        print (user_num1)
         
-def ex2(ddc, user_num1):
-    ddc = ddc + 1
+def ex2():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
     if ddc != 2:
         user_num1 = 2
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 2
+        print (user_num1)
         
-def ex3(ddc, user_num1):
-    ddc = ddc + 1
+def ex3():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
     if ddc != 2:
         user_num1 = 3
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 3
+        print (user_num1)
         
-def ex4(ddc, user_num1):
-    ddc = ddc + 1
+def ex4():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     if ddc != 2:
         user_num1 = 4
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 4
+        print (user_num1)
         
-def ex5(ddc, user_num1):
-    ddc = ddc + 1
+def ex5():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
     if ddc != 2:
         user_num1 = 5
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 5
+        print (user_num1)
         
-def ex6(ddc, user_num1):
-    ddc = ddc + 1
+def ex6():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
 
     if ddc != 2:
         user_num1 = 6
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 6
+        print (user_num1)
         
-def ex7(ddc, user_num1):
-    ddc = ddc + 1
+def ex7():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
     if ddc != 2:
         user_num1 = 7
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 7
+        print (user_num1)
         
-def ex8(ddc, user_num1):
-    ddc = ddc + 1
+def ex8():
+    global ddc
+    global user_num1
+    if ddc != 2:
+        ddc = ddc + 1
 
     
     if ddc != 2:
         user_num1 = 8
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 8
+        print (user_num1)
         
-def ex9(ddc, user_num1):
-    ddc = ddc + 1
+def ex9():
+    global ddc
+    global user_num1
+    if ddc != 2:
+    
+        ddc = ddc + 1
 
     
     if ddc != 2:
         user_num1 = 9
+        print(user_num1)
     elif ddc == 2:
-        user_num1_ram = user_num1 * 10
+        user_num1 = user_num1 * 10
 
-        user_num1_ram = user_num1_ram + 1
-        print (user_num1_ram)
+        user_num1 = user_num1 + 9
+        print (user_num1)
         
+
+
+
+
+
 height=3
 width=5
 root = Tk()
 
 root.geometry('300x300')
 
-num1 = Button(root, text="1", height=height, width=width, command = ex1(ddc, user_num1))
-num2 = Button(root, text="2", height=height, width=width, command = ex2(ddc, user_num1))
-num3 = Button(root, text="3", height=height, width=width, command = ex3(ddc, user_num1))
-num4 = Button(root, text="4", height=height, width=width, command = ex4(ddc, user_num1))
-num5 = Button(root, text="5", height=height, width=width, command = ex5(ddc, user_num1))
-num6 = Button(root, text="6", height=height, width=width, command = ex6(ddc, user_num1))
-num7 = Button(root, text="7", height=height, width=width, command = ex7(ddc, user_num1))
-num8 = Button(root, text="8", height=height, width=width, command = ex8(ddc, user_num1))
-num9 = Button(root, text="9", height=height, width=width, command = ex9(ddc, user_num1))
+num1 = Button(root, text="1", height=height, width=width, command = ex1)
+num2 = Button(root, text="2", height=height, width=width, command = ex2)
+num3 = Button(root, text="3", height=height, width=width, command = ex3)
+num4 = Button(root, text="4", height=height, width=width, command = ex4)
+num5 = Button(root, text="5", height=height, width=width, command = ex5)
+num6 = Button(root, text="6", height=height, width=width, command = ex6)
+num7 = Button(root, text="7", height=height, width=width, command = ex7)
+num8 = Button(root, text="8", height=height, width=width, command = ex8)
+num9 = Button(root, text="9", height=height, width=width, command = ex9)
 num0 = Button(root, text="0", height=3, width=12, command = ex0)
 
 num1.place(x=100, y=100)
