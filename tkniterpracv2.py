23e29b69fb92e3bca7336fa2dc67ba5238b297a9
DreiLuna <54549805+DreiLuna@users.noreply.github.com>
2022-02-09 11:28:56 -0600

    first

---
+++ tkniterpracv2.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import tkinter.font as tkFont
+
+def test():
+    print("it worked")
+def ex1():
+    a
+def ex2():
+def ex3():
+def ex4():
+def ex5():
+def ex6():
+def ex7():
+def ex8():
+def ex9():
+height=3
+width=5
+root = Tk()
+
+root.geometry('300x300')
+
+num1 = Button(root, text="1", height=height, width=width, command = ex1)
+num2 = Button(root, text="2", height=height, width=width, command = ex2)
+num3 = Button(root, text="3", height=height, width=width, command = ex3)
+num4 = Button(root, text="4", height=height, width=width, command = ex4)
+num5 = Button(root, text="5", height=height, width=width, command = ex5)
+num6 = Button(root, text="6", height=height, width=width, command = ex6)
+num7 = Button(root, text="7", height=height, width=width, command = ex7)
+num8 = Button(root, text="8", height=height, width=width, command = ex8)
+num9 = Button(root, text="9", height=height, width=width, command = ex9)
+
+num1.place(x=100, y=100)
+
+root.mainloop()
