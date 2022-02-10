4efb6139a6ec96fd2b46d9249a54c6664a4f8418
Drei Luna <dreifire@gmail.com>
2022-02-09 20:43:09 -0600

    calc com

--- tkniterpracv2.py
+++ tkniterpracv2.py
@@ -1,14 +1,22 @@
+from doctest import Example
+from math import expm1
 from tkinter import *
 import tkinter.font as tkFont
 
 global ddc
 ddc = 0
+global ddc2
+ddc2 = 0
 global user_num1
 user_num1= 0
-
+global second_num
+second_num = 0
+#! 0
 def ex0():
     global ddc
+    global ddc2
     global user_num1
+    global user_num2
     if ddc != 2:
         ddc = ddc + 1
 
@@ -24,156 +32,347 @@ def ex0():
         user_num1 = user_num1 + 0
         print (user_num1)
         
+
+#! 1
 def ex1():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-
-    if ddc != 2:
-        user_num1 = 1
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 1
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 1
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 1
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 1
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 1
+            print (user_num1)
+
+#!2
 def ex2():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-    if ddc != 2:
-        user_num1 = 2
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 2
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 2
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 2
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 2
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 2
+            print (user_num1)
+#!3
 def ex3():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-    if ddc != 2:
-        user_num1 = 3
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 3
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 3
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 3
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 3
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 3
+            print (user_num1)
+
+#! 4
 def ex4():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    if ddc != 2:
-        user_num1 = 4
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 4
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 4
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 4
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 4
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 4
+            print (user_num1)
+
+#! 5
 def ex5():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-    if ddc != 2:
-        user_num1 = 5
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 5
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 5
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 5
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 5
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 5
+            print (user_num1)
+#! 6
 def ex6():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-
-    if ddc != 2:
-        user_num1 = 6
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 6
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 6
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 6
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 6
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 6
+            print (user_num1)
+#! 7
 def ex7():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-    if ddc != 2:
-        user_num1 = 7
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 7
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 7
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 7
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 7
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 7
+            print (user_num1)
+#! 8
 def ex8():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-    if ddc != 2:
-        user_num1 = 8
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 8
-        print (user_num1)
         
+
+        if ddc != 2:
+            user_num1 = 8
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 8
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 8
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 8
+            print (user_num1)
+#! 9
 def ex9():
     global ddc
+    global ddc2
     global user_num1
-    if ddc != 2:
-    
-        ddc = ddc + 1
+    global user_num2
+    if second_num != 1:
+        if ddc != 2:
+            ddc = ddc + 1
 
-    
-    if ddc != 2:
-        user_num1 = 9
-        print(user_num1)
-    elif ddc == 2:
-        user_num1 = user_num1 * 10
-
-        user_num1 = user_num1 + 9
-        print (user_num1)
         
 
+        if ddc != 2:
+            user_num1 = 9
+            print(user_num1)
+        elif ddc == 2:
+            user_num1 = user_num1 * 10
+
+            user_num1 = user_num1 + 9
+            print (user_num1)
+    elif second_num == 1:
+        if ddc2 != 2:
+            ddc2 = ddc2 + 1
+
+        
+
+        if ddc2 != 2:
+            user_num2 = 9
+            print(user_num2)
+        elif ddc2 == 2:
+            user_num2 = user_num2 * 10
+
+            user_num2 = user_num2 + 9
+            print (user_num1)
 
 
+def exa():
+    global second_num
+    second_num = 1
 
+    op = "add"
+def exs():
+    global second_num
+    second_num = 1
+    user_num1
+def exm():
+    global second_num
+    second_num = 1
+    user_num1
+def exd():
+    global second_num
+    second_num = 1
+    user_num1
+def exmd():
+    global second_num
+    second_num = 1
+    user_num1
+def exp():
+    global second_num
+    second_num = 1
+    user_num1
+
+
+def ent():
+    display
 
 height=3
 width=5
@@ -190,17 +389,35 @@ num6 = Button(root, text="6", height=height, width=width, command = ex6)
 num7 = Button(root, text="7", height=height, width=width, command = ex7)
 num8 = Button(root, text="8", height=height, width=width, command = ex8)
 num9 = Button(root, text="9", height=height, width=width, command = ex9)
-num0 = Button(root, text="0", height=3, width=12, command = ex0)
+num0 = Button(root, text="0", height=height, width=width, command = ex0)
 
-num1.place(x=100, y=100)
-num2.place(x=150, y=100)
-num3.place(x=200, y=100)
-num4.place(x=100, y=150)
-num5.place(x=150, y=150)
-num6.place(x=200, y=150)
-num7.place(x=100, y=200)
-num8.place(x=150, y=200)
-num9.place(x=200, y=200)
-num0.place(x=100, y=260)
+opa = Button(root, text="+", height=height, width=width, command = exa)
+ops = Button(root, text="-", height=height, width=width, command = exs)
+opm = Button(root, text="*", height=height, width=width, command = exm)
+opd = Button(root, text="/", height=height, width=width, command = exd)
+opmd = Button(root, text="%", height=height, width=width, command = exmd)
+opp = Button(root, text="^", height=height, width=width, command = exp)
+
+enter = Button(root, text="^", height=height, width=width, command = ent)
+
+
+opa.place(x=160, y=10)
+ops.place(x=160, y=70)
+opm.place(x=160, y=130)
+opd.place(x=160, y=190)
+opmd.place(x=60, y=190)
+opp.place(x=110, y=190)
+
+
+num1.place(x=10, y=10)
+num2.place(x=60, y=10)
+num3.place(x=110, y=10)
+num4.place(x=10, y=70)
+num5.place(x=60, y=70)
+num6.place(x=110, y=70)
+num7.place(x=10, y=130)
+num8.place(x=60, y=130)
+num9.place(x=110, y=130)
+num0.place(x=10, y=190)
 
 root.mainloop()
